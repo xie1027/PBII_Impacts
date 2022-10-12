4d6909a12d51dc76b2b484038f33c1c6fa7ced5d
Wei Chen <victoriaweichen7@gmail.com>
2022-10-12 02:07:21 -0400

    Updates

---
+++ precache-manifest.7316ec0e7daddc1825ddec1b273a6118.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da7fd52f0192bf1f77720233fdd041c",
+    "url": "/PBII_Impacts/index.html"
+  },
+  {
+    "revision": "c32573c1f8b6fed90486",
+    "url": "/PBII_Impacts/static/css/2.0b5659ac.chunk.css"
+  },
+  {
+    "revision": "6021b1fa0101ca4b3e50",
+    "url": "/PBII_Impacts/static/css/main.d75b6272.chunk.css"
+  },
+  {
+    "revision": "c32573c1f8b6fed90486",
+    "url": "/PBII_Impacts/static/js/2.d18fdc5b.chunk.js"
+  },
+  {
+    "revision": "11f86ddc566c30b6cad223d8a078311f",
+    "url": "/PBII_Impacts/static/js/2.d18fdc5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6021b1fa0101ca4b3e50",
+    "url": "/PBII_Impacts/static/js/main.2d1779b6.chunk.js"
+  },
+  {
+    "revision": "12c6850daf25bea190f1",
+    "url": "/PBII_Impacts/static/js/runtime-main.95b2d5da.js"
+  },
+  {
+    "revision": "259d6ecf9142b73c4953cb2f36c24efb",
+    "url": "/PBII_Impacts/static/media/ABBALegend.259d6ecf.png"
+  },
+  {
+    "revision": "fc5969d4bc0f2dd4230b537c64ae53e0",
+    "url": "/PBII_Impacts/static/media/ABBA_Dataset.fc5969d4.zip"
+  },
+  {
+    "revision": "c13fa0899ff1182ab56946d9f8d2463f",
+    "url": "/PBII_Impacts/static/media/AOI.c13fa089.zip"
+  },
+  {
+    "revision": "6664085334b4fc5b97b19c05abbb48d7",
+    "url": "/PBII_Impacts/static/media/Beijing.66640853.jpg"
+  },
+  {
+    "revision": "92a84ec95ef9d7a20ef1cbb42e7baa5c",
+    "url": "/PBII_Impacts/static/media/Olympics.92a84ec9.jpg"
+  },
+  {
+    "revision": "f8041b0394cc02fa9256be370287c198",
+    "url": "/PBII_Impacts/static/media/Watersheds.f8041b03.zip"
+  },
+  {
+    "revision": "a831c7165bb54c334bbed2ff05b76a89",
+    "url": "/PBII_Impacts/static/media/af.a831c716.png"
+  },
+  {
+    "revision": "20f19f9e5a7f8298bb041346bf62e25d",
+    "url": "/PBII_Impacts/static/media/airplane.20f19f9e.svg"
+  },
+  {
+    "revision": "eefba0acd878390c34d3a6ad0ac1e134",
+    "url": "/PBII_Impacts/static/media/bayBridge.eefba0ac.jpg"
+  },
+  {
+    "revision": "db2bf4cd8f729b58f914c26c7ae70489",
+    "url": "/PBII_Impacts/static/media/city.db2bf4cd.svg"
+  },
+  {
+    "revision": "32406ab8abb5edf5820cf907b0d1a799",
+    "url": "/PBII_Impacts/static/media/cw.32406ab8.png"
+  },
+  {
+    "revision": "a92836372bfc0bfbe07a48a5627e01f1",
+    "url": "/PBII_Impacts/static/media/doubleLeaf.a9283637.svg"
+  },
+  {
+    "revision": "f9068c05277360aad1a5245e86e3aa2a",
+    "url": "/PBII_Impacts/static/media/exampleLegend4.f9068c05.png"
+  },
+  {
+    "revision": "19a7859f33b2bd302f2dcc1ac3ffeeca",
+    "url": "/PBII_Impacts/static/media/exampleRaster.19a7859f.tif"
+  },
+  {
+    "revision": "8ce20f1f4ff4c5658bd9c1fa5075dcbe",
+    "url": "/PBII_Impacts/static/media/forest.8ce20f1f.svg"
+  },
+  {
+    "revision": "137612a416f30b34f59ad10dc2d81f0b",
+    "url": "/PBII_Impacts/static/media/halloween.137612a4.jpg"
+  },
+  {
+    "revision": "b1b2edf8be2c33bf750558378bda5ed8",
+    "url": "/PBII_Impacts/static/media/headerImg.b1b2edf8.png"
+  },
+  {
+    "revision": "18bb21a79bc8d8119a8063a01c34bb7a",
+    "url": "/PBII_Impacts/static/media/herrin.18bb21a7.jpg"
+  },
+  {
+    "revision": "6fac5a756d01617d54eec174a96366dc",
+    "url": "/PBII_Impacts/static/media/house.6fac5a75.svg"
+  },
+  {
+    "revision": "630971fb41a0fe2f816a295bf9eff547",
+    "url": "/PBII_Impacts/static/media/leaf.630971fb.svg"
+  },
+  {
+    "revision": "087bbae76bbc32a657f792dd3c66498a",
+    "url": "/PBII_Impacts/static/media/minneapolis.087bbae7.jpg"
+  },
+  {
+    "revision": "1053ed6f79c0f1fb66402d40cb45e8c7",
+    "url": "/PBII_Impacts/static/media/natCapAdventures.1053ed6f.zip"
+  },
+  {
+    "revision": "c6a9c80e61161204827462d8e1213897",
+    "url": "/PBII_Impacts/static/media/natcap.c6a9c80e.png"
+  },
+  {
+    "revision": "8ac0faef57d3116b718290f220060876",
+    "url": "/PBII_Impacts/static/media/nationalParks.8ac0faef.zip"
+  },
+  {
+    "revision": "c2ce20e8789eac9eb06bff205a02bb51",
+    "url": "/PBII_Impacts/static/media/no-profile-img.c2ce20e8.png"
+  },
+  {
+    "revision": "80b85c0259678788d670c1b61c38b5eb",
+    "url": "/PBII_Impacts/static/media/norrkoping.80b85c02.png"
+  },
+  {
+    "revision": "2e23d06d99569744b010926cc623c9b9",
+    "url": "/PBII_Impacts/static/media/puertoMaldonado.2e23d06d.jpg"
+  },
+  {
+    "revision": "d2249e85847a392570312e6aae1c5f47",
+    "url": "/PBII_Impacts/static/media/redmond.d2249e85.jpg"
+  },
+  {
+    "revision": "0f5d2aa809a1ea3ab957296ecc61f7ed",
+    "url": "/PBII_Impacts/static/media/shapefile.0f5d2aa8.zip"
+  },
+  {
+    "revision": "a2778aa91b27776c6e1d041197904b46",
+    "url": "/PBII_Impacts/static/media/stockholm.a2778aa9.jpg"
+  },
+  {
+    "revision": "e734bea51caca1f902c41a483ff793e5",
+    "url": "/PBII_Impacts/static/media/wyoming.e734bea5.jpg"
+  }
+]);
